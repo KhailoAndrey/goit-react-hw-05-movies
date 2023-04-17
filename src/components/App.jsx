4dfb8eab9158d92a600f4ff8de6653662db20012
KhailoAndrey <hap512@gmail.com>
2023-04-18 02:17:54 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,8 @@ import Layout from './Layout';
 import { lazy } from 'react';
 import NotFound from 'pages/NotFound';
 
-const Home = lazy(() => import('../pages/home'));
-const Movies = lazy(() => import('../pages/movies'));
+const Home = lazy(() => import('../pages/Home'));
+const Movies = lazy(() => import('../pages/Movies'));
 const MovieDetails = lazy(() => import('../pages/MovieDetails'));
 const Cast = lazy(() => import('../components/Cast'));
 const Reviews = lazy(() => import('../components/Reviews'));
